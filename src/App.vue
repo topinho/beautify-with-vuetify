0840e02a96f08929b7be678ad150244fd0e21707
German Torres <topinho1806@gmail.com>
2021-10-14 17:43:17 -0300

    Agregar Router. Pasar Login a Views. Agregar Dashboard.

--- src/App.vue
+++ src/App.vue
@@ -4,49 +4,31 @@
       <v-app-bar app color="primary" dark>
         <v-toolbar-title>Beautify With Vuetify</v-toolbar-title>
         <v-spacer />
-        <v-btn text rounded>Home</v-btn>
-        <v-btn text rounded>Login</v-btn>
+        <v-btn
+          v-for="link in links"
+          :key="`${link.label}-header-link`"
+          text
+          rounded
+          :to="link.url"
+        >
+          {{ link.label }}
+        </v-btn>
       </v-app-bar>
       <v-content>
-        <!-- Login -->
-        <v-card width="400" class="mx-auto mt-5">
-          <v-card-title>
-            <h1 class="display-1">Login</h1>
-          </v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                label="Username"
-                prepend-icon="mdi-accoundt-circle"
-              />
-              <v-text-field
-                :type="showPassword ? 'text' : 'password'"
-                label="Password"
-                prepend-icon="mdi-lock"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="showPassword = !showPassword"
-              />
-            </v-form>
-          </v-card-text>
-          <v-divider />
-          <v-card-actions>
-            <v-btn color="success">Registro</v-btn>
-            <v-spacer />
-            <v-btn color="info">Login</v-btn>
-          </v-card-actions>
-        </v-card>
+        <router-view></router-view>
       </v-content>
       <v-footer color="primary lighten-1" padless>
         <v-layout justify-center wrap>
           <v-btn
             v-for="link in links"
-            :key="link"
+            :key="`${link.label}-footer-link`"
             color="white"
             text
             rounded
             class="my-2"
+            :to="link.url"
           >
-            {{ link }}
+            {{ link.label }}
           </v-btn>
           <v-flex primary lighten-2 py-4 text-center white--text xs12>
             {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
@@ -69,10 +51,19 @@ export default {
 
   data () {
     return {
-      showPassword: false,
       links: [
-        'Home',
-        'Login'
+        {
+          label: 'Home',
+          url: '/'
+        },
+        {
+          label: 'Login',
+          url: '/login'
+        },
+        {
+          label: 'Dashboard',
+          url: '/dashboard'
+        }
       ]
     }
   }
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          label="Username"
+          prepend-icon="mdi-accoundt-circle"
+        />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider />
+    <v-card-actions>
+      <v-btn color="success">Registro</v-btn>
+      <v-spacer />
+      <v-btn color="info">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
